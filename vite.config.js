10fca2382abf6d5f10aaf717d85d3f97a3df58f4
HAMMAOUI388 <fatimzahrahammaoui388@gmail.com>
2025-07-13 17:30:12 +0100

    Fix Vite manifest.json location

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [laravel({
-        input: ['resources/js/app.js'],
+        input: ['resources/css/app.css', 'resources/js/app.js'],
         refresh: true,
     })],
     build: {
@@ -13,5 +13,9 @@ export default defineConfig({
             input: 'resources/js/app.js',
         },
     },
+    server: {
+        https: true,
+    },
 });
+
 
