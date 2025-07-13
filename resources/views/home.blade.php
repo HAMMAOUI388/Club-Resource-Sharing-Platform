7467abc0f53f50ef0fbd74fb65ae2cfefcae4a4e
HAMMAOUI388 <fatimzahrahammaoui388@gmail.com>
2025-07-13 16:59:56 +0100

    Fix Vite manifest.json location

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Industry 4.0</title>
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ secure_asset('favicon.ico') }}" type="image/x-icon">
 
     {{-- Vite-built CSS & JS --}}
     @vite(['resources/js/app.js'])
 
     {{-- Raw CSS files --}}
-    <link rel="stylesheet" href="{{ asset('css/event.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/post.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/ressources.css') }}">
-    <link rel="stylesheet" href="{{ asset('slider/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/event.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/post.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/ressources.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('slider/style.css') }}">
 </head>
 
 <body>
