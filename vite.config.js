cbf9b7391ca539db32dd937437905ff186326feb
HAMMAOUI388 <fatimzahrahammaoui388@gmail.com>
2025-07-19 15:44:23 +0100

    Fix: enforce HTTPS Vite assets

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
-            buildDirectory: 'build', // 💥 important
         }),
     ],
-    build: {
-        outDir: 'public/build',
-        emptyOutDir: true,
-        manifest: true,
-    },
 });
